1d0a78646e020f42dca6a2118e72a4f8a513f0bc
rodney <rjkahane@gmail.com>
2018-07-24 14:17:07 -0400

    update

--- heapimplementation/src/heapimplementation/App.java
+++ heapimplementation/src/heapimplementation/App.java
@@ -3,10 +3,26 @@ package heapimplementation;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Heap heap = new Heap(11);
+		
+		heap.insert(82);
+		heap.insert(51);
+		heap.insert(63);
+		heap.insert(70);
+		heap.insert(37);
+		heap.insert(10);
+		heap.insert(43);
+		heap.insert(27);
+		heap.insert(55);
+		heap.insert(34);
+		heap.insert(30);
 		
 		
-	}
+		
+		heap.dispHeap();
+		heap.remove();
+		System.out.println("");
+		heap.dispHeap();
+	}//end main
 
-}
+}//end class
--- heapimplementation/src/heapimplementation/Heap.java
+++ heapimplementation/src/heapimplementation/Heap.java
@@ -8,53 +8,81 @@ public class Heap {
 	private Node[] heapArray;
 	private int maxSize;  //size of the array
 	private int currentSize;  //number of nodes in the array
-	
+
 	public Heap(int size) {
 		this.maxSize = size;
 		currentSize = 0;
 		heapArray = new Node[size];  //actually creates array
 	}
-	
+
 	public int getSize() {
 		return currentSize;
 	}
-	
+
 	public boolean isEmpty() {
 		return(currentSize == 0);
 	}
-	
+
 	public boolean insert(int key) {
 		if(currentSize == maxSize) {
 			return false;
 		}
-		
+
 		Node newNode = new Node(key);
 		heapArray[currentSize] = newNode;
-		
+
 		trickleUp(currentSize);		
 		currentSize++;
-		
+
 		return true;	
 	}//end insert()
-	
+
 	public void trickleUp(int idx){
 		int parentIdx = (idx-1)/2;
 		Node nodeToInsert = heapArray[idx];
-		
+
 		//loop as long as we havent reached the root and key of nodeToInsert's parent is less than new node
 		while(idx > 0 && heapArray[parentIdx].getKey() < nodeToInsert.getKey()) {
 			heapArray[idx] = heapArray[parentIdx]; //move parent down
 			idx = parentIdx;
 			parentIdx = (parentIdx-1)/2;
 		}
-		
+
 		heapArray[idx]=nodeToInsert;
-		
+
 	}//end trickleUp()
-	
+
 	public boolean remove() {
-		
+		int removeIdx=heapArray.length-1;
+		int idx=0;
+		int lChild=(2*idx)+1;
+		int rChild=(2*idx)+2;
+		Node tmpNode;
+
+
+		heapArray[0] = heapArray[removeIdx];
+
+		for(int i=0;i<heapArray.length;i++) {
+			if(heapArray[lChild].getKey() > heapArray[idx].getKey()) {
+				tmpNode = heapArray[idx];
+				heapArray[idx] = heapArray[lChild];
+				heapArray[lChild] = heapArray[idx];
+
+			} else if(heapArray[rChild].getKey() > heapArray[idx].getKey()) {
+				tmpNode = heapArray[idx];
+				heapArray[idx] = heapArray[rChild];
+				heapArray[rChild] = heapArray[idx];
+			}
+
+		}
+		currentSize--;
 		return true;
+	}//end remove()
+
+	public void dispHeap() {
+		for(int i=0;i<heapArray.length;i++) {
+			System.out.print(heapArray[i].getKey()+", ");
+		}
 	}
-	
+
 }//end class
